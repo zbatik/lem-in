1a7b0a09473f7e9441e7a4d21e34505f493c5528
Zack BATIK <zbatik@e4r21p4.wethinkcode.co.za>
2018-08-12 16:34:43 +0200

    bla

--- lemin.h
+++ lemin.h
@@ -6,17 +6,22 @@
 /*   By: zbatik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/10 11:03:22 by zbatik            #+#    #+#             */
-/*   Updated: 2018/08/10 11:34:50 by zbatik           ###   ########.fr       */
+/*   Updated: 2018/08/12 16:34:09 by zbatik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LEMIN_H
 # define LEMIN_H
 
+#include <stdlib.h>
+#include <fcntl.h>
+
 typedef struct	s_lem
 {
 	int		num_rooms;
-	t_graph	map;
+	int		**map;
+	char	**raw;
+
 }				t_lem;
 
 typedef struct	s_graph
--- main.c
+++ main.c
@@ -6,15 +6,34 @@
 /*   By: zbatik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/10 11:00:47 by zbatik            #+#    #+#             */
-/*   Updated: 2018/08/10 11:34:47 by zbatik           ###   ########.fr       */
+/*   Updated: 2018/08/12 16:34:05 by zbatik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	init_data()
 {
+	t_dblist	*rooms;
+	char		*gnl;
+	char		room_name;
+	int			room_num;
+	
+	rooms = NULL;
+	room_num = 0;
+	while (get_next_line(0, &gnl) != 0);
+	{
+		if (ft_countc(gnl, ' ') == 2)
+		{
+			room_name = ft_strsub(gnl, 0, ft_indexcin(gnl, ' '));
+			ft_lstdbadd(&rooms, ft_lstdbnew(room_name, room_number++));
+			ft_strdel(room_name);
+		}
+		else if (ft_countc(gnl, ' ') == 0)
+		{
 
+		}
+}
 
 int main(void)
 {
-	
+
 }
